da63d18c5b2e00d726d68dd154247d9c5da29f85
Ecob1 <78009340+Ecob1@users.noreply.github.com>
2024-05-13 20:11:47 -0700

    Add more lines

--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -1,1 +1,16 @@
-export class 
+// import { Component } from "@angular/core";
+
+// @Component({
+//     selector:'app-server',
+//     templateUrl:'../contacts/contacts.component.html'
+// })
+export class Contact {
+  constructor(
+    public id: number,
+    public name: string,
+    public email: string,
+    public phone: string,
+    public imageUrl: string,
+    public group?: Contact[]
+  ) {}
+}
